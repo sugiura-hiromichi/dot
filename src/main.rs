396d324a392b4217ea21ddf9f6e6318a42151f95
sugiura-hiromichi <sugiura130418@icloud.com>
2023-01-06 16:23:20 +0900

    fix(git clone) fix url

--- src/main.rs
+++ src/main.rs
@@ -54,7 +54,7 @@ fn main() {
 				Ok(_,) => {
 					//  need to clone
 					println!("Clone your dotfiles directory.");
-					sh_cmd!("git", ["clone".to_string(), format!("https://github.com/{REPOSITORY}/")]);
+					sh_cmd!("git", ["clone".to_string(), format!("https://github.com/{REPOSITORY}")]);
 				},
 				Err(e,) => {
 					// exit
